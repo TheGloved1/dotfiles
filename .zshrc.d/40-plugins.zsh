5efd0fcc6405f4296ee08103fcf5ee27d66a4231
TheGloved1 <gloves1229@gmail.com>
2026-08-16 00:47:09 -0500

    add(40-plugins.zsh): update 40-plugins.zsh

---
+++ .zshrc.d/40-plugins.zsh
@@ -0,0 +1,3 @@
+# Zsh plugins (standalone, not tied to oh-my-zsh)
+source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
