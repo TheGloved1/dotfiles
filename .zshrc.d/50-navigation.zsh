5e82a2ab340e870e7d526f5f8a90bf7e84b31b2c
TheGloved1 <gloves1229@gmail.com>
2026-08-16 00:47:09 -0500

    add(50-navigation.zsh): update 50-navigation.zsh

---
+++ .zshrc.d/50-navigation.zsh
@@ -0,0 +1,14 @@
+# Zoxide
+eval "$(zoxide init zsh)"
+
+alias cd="z"
+
+# yazi: cd into last browsed directory on exit
+function y() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(cat "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+        cd -- "$cwd"
+    fi
+    rm -f -- "$tmp"
+}
