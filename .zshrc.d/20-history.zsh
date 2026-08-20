02cae2647b9e782f4c427f5834af774514a7a8a4
TheGloved1 <gloves1229@gmail.com>
2026-08-20 16:20:37 -0500

    update(20-history.zsh): update 20-history.zsh

--- .zshrc.d/20-history.zsh
+++ .zshrc.d/20-history.zsh
@@ -3,3 +3,9 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 setopt appendhistory
+
+# Up/Down arrows filter history by current input (prefix match)
+bindkey '^[[A' history-search-backward
+bindkey '^[[B' history-search-forward
+bindkey '^[OA' history-search-backward
+bindkey '^[OB' history-search-forward
