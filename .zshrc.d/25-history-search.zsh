48ac0d786d199cbb75cd1011c2dfb24ba75d5717
TheGloved1 <gloves1229@gmail.com>
2026-08-20 14:21:07 -0500

    add(25-history-search.zsh): update 25-history-search.zsh

---
+++ .zshrc.d/25-history-search.zsh
@@ -0,0 +1,5 @@
+# Up/Down arrows filter history by current input (prefix match)
+bindkey '^[[A' history-search-backward
+bindkey '^[[B' history-search-forward
+bindkey '^[OA' history-search-backward
+bindkey '^[OB' history-search-forward
