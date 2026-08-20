8f2609e1202efeb493f624098898db668cdf61b4
TheGloved1 <gloves1229@gmail.com>
2026-08-20 16:20:37 -0500

    remove(25-history-search.zsh): update 25-history-search.zsh

--- .zshrc.d/25-history-search.zsh
+++
@@ -1,5 +0,0 @@
-# Up/Down arrows filter history by current input (prefix match)
-bindkey '^[[A' history-search-backward
-bindkey '^[[B' history-search-forward
-bindkey '^[OA' history-search-backward
-bindkey '^[OB' history-search-forward
