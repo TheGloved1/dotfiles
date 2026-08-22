93e99df76d99655b0c13c4b2845d104de8aab026
TheGloved1 <gloves1229@gmail.com>
2026-08-22 15:14:52 -0500

    remove(hypr): update hypr

--- .config/hypr/modules/defaults.lua
+++
@@ -1,7 +0,0 @@
-DEFAULTS = DEFAULTS or {}
-
-DEFAULTS.edit = "nvim"
-DEFAULTS.visual = "nvim"
-DEFAULTS.term = "kitty"
-DEFAULTS.files = "yazi"
-DEFAULTS.search_engine = "https://duckduckgo.com/?q="
