7421bbfcfa4c7b2fabe66ba7a52d4863dfdb28c2
TheGloved1 <gloves1229@gmail.com>
2026-08-28 01:07:08 -0500

    remove(40-plugins.zsh): update 40-plugins.zsh

--- .zshrc.d/40-plugins.zsh
+++
@@ -1,3 +0,0 @@
-# Zsh plugins (standalone, not tied to oh-my-zsh)
-source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
