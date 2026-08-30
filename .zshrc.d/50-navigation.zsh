9b50928b34088b78683ffb9811b75e17c69a7d03
TheGloved1 <gloves1229@gmail.com>
2026-08-30 08:33:11 -0500

    update(50-navigation.zsh): update 50-navigation.zsh

--- .zshrc.d/50-navigation.zsh
+++ .zshrc.d/50-navigation.zsh
@@ -4,14 +4,17 @@ eval "$(zoxide init zsh)"
 alias cd="z"
 
 # yazi: cd into last browsed directory on exit
-function y() {
+# shared wrapper - used by both `y` and `yazi` so `yazi` is default in interactive shells
+__yazi_cd() {
     local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-    yazi "$@" --cwd-file="$tmp"
+    command yazi "$@" --cwd-file="$tmp"
     if cwd="$(cat "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
         cd -- "$cwd"
     fi
     rm -f -- "$tmp"
 }
+function y() { __yazi_cd "$@" }
+function yazi() { __yazi_cd "$@" }
 
 # Ctrl+E: run y (yazi file manager)
 bindkey -s '^E' 'y
