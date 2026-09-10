353194430e9d732f8eab8a931bf4f32949dee3f0
TheGloved1 <gloves1229@gmail.com>
2026-09-09 20:12:36 -0500

    update(20-history.zsh): update 20-history.zsh

--- .zshrc.d/20-history.zsh
+++ .zshrc.d/20-history.zsh
@@ -4,8 +4,19 @@ HISTSIZE=10000
 SAVEHIST=10000
 setopt appendhistory
 
-# Up/Down arrows filter history by current input (prefix match)
-bindkey '^[[A' history-search-backward
-bindkey '^[[B' history-search-forward
-bindkey '^[OA' history-search-backward
-bindkey '^[OB' history-search-forward
+# Up/Down arrows: prefix-filter history, but move within multiline buffers
+# up-line-or-search / down-line-or-search = multiline-aware wrapper around history-search-*
+autoload -Uz up-line-or-search down-line-or-search
+for _km in viins main; do
+  bindkey -M $_km '^[[A' up-line-or-search
+  bindkey -M $_km '^[[B' down-line-or-search
+  bindkey -M $_km '^[OA' up-line-or-search
+  bindkey -M $_km '^[OB' down-line-or-search
+done
+for _km in vicmd; do
+  bindkey -M $_km '^[[A' up-line-or-search
+  bindkey -M $_km '^[[B' down-line-or-search
+  bindkey -M $_km '^[OA' up-line-or-search
+  bindkey -M $_km '^[OB' down-line-or-search
+done
+unset _km
